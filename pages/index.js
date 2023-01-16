c19aed76b68b15ccb5c9b1bd1be31c50fb6d90c1
Jarek <jarekjanas95@gmail.com>
2023-01-16 23:47:09 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// import Image from 'next/image'
+
+export default function Home() {
+  return <h2>piekarnia</h2>;
+}
+
+{
+  /* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */
+}

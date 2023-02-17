5348a65edd9a82be9d6f8fa22f6abf9f85df2ea2
Jarek <jarekjanas95@gmail.com>
2023-02-17 22:53:47 +0100

    commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -440,10 +440,7 @@ const Wrapper2 = styled.div`
   }
   .languages {
     position: absolute;
-    bottom: 7vh;
-    bottom: 7dvh;
-    /* bottom: 2vh;
-    bottom: 2dvh; */
+    bottom: 2vh;
     /* left: 50%;
     transform: translateX(-50%); */
     right: 2vw;
